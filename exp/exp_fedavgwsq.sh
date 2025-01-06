e236505a984841dbd2ae376c33c7e6c7c26d978b
gymlab <swkim@pknu.ac.kr>
2025-01-06 11:16:40 +0900

    updates

--- exp/exp_fedavgwsq.sh
+++ exp/exp_fedavgwsq.sh
@@ -1,14 +1,14 @@
 CUDA_VISIBLE_DEVICES=0
-DATASET=cifar100
+DATASET=tinyimagenet
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
 ALPHA=0.3
-NBITS=4
+NBITS=2
 
-python3 federated_train.py client=base server=base visible_devices=\'3\' exp_name=FedAvgWSQInitU_"$ALPHA"_"B$NBITS" \
-dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
+python3 federated_train.py client=base server=base visible_devices=\'2\' exp_name=FedAvgWSQ_"$ALPHA"_"B$NBITS" \
+dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.02 \
 quantizer=WSQ quantizer.wt_bit=${NBITS} \
-batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant" \
+batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant2" \
 # split.mode=iid
