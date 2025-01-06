2bf4853ba04f00957d9efa61308f54742f20df5e
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-01-06 11:15:30 +0900

    updates

--- exp/exp_fedavgwsq.sh
+++ exp/exp_fedavgwsq.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0
-DATASET=cifar100
+DATASET=tinyimagenet
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
@@ -7,8 +7,8 @@ fi
 ALPHA=0.3
 NBITS=8
 
-python3 federated_train.py client=base server=base visible_devices=\'0\' exp_name=FedAvgWSQInitU_"$ALPHA"_"B$NBITS" \
-dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
+python3 federated_train.py client=base server=base visible_devices=\'1\' exp_name=FedAvgWSQ_"$ALPHA"_"B$NBITS" \
+dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.02 \
 quantizer=WSQ quantizer.wt_bit=${NBITS} \
-batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant" \
+batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant2" \
 # split.mode=iid
