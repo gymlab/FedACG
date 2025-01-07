0abd9638842250069fa981e000d5ce73ba08fd85
성열 <qksksktjdduf@naver.com>
2025-01-07 16:36:48 +0900

    updates

---
+++ exp/exp_fedavgws_quant2.bat
@@ -0,0 +1,30 @@
+@echo off
+
+cd ..\
+
+set DATASETS=CIFAR10 CIFAR100
+set BITS=1 2 4 8
+set METHODS=PAQ HQ
+set ALPHA=0.3
+set PARTICIPATION_RATE=0.02
+
+for %%D in (%DATASETS%) do (
+    if "%%D" == "tinyimagenet" (
+        set BATCH_SIZE=100
+    ) else (
+        set BATCH_SIZE=50
+    )
+
+    for %%M in (%METHODS%) do (
+        for %%B in (%BITS%) do (
+            for %%N in (100 500) do (
+                echo Running experiment with DATASET=%%D, METHOD=%%M, BITS=%%B, NUM_CLIENTS=%%N
+                python federated_train.py client=base server=base quantizer=%%M ^
+                exp_name=Fed%%M_%%D_B%%B_C%%N dataset=%%D trainer.num_clients=%%N ^
+                split.alpha=%ALPHA% trainer.participation_rate=%PARTICIPATION_RATE% ^
+                batch_size=%BATCH_SIZE% wandb=True model=resnet18_WS quantizer.wt_bit=%%B ^
+                project="FedAVG_WS_Quant"
+            )
+        )
+    )
+)
