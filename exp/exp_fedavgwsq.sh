ae22ee88d3b50a42f0b1ac86741f8c2bd3216892
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-01-24 14:44:30 +0900

    Updates

--- exp/exp_fedavgwsq.sh
+++ exp/exp_fedavgwsq.sh
@@ -6,7 +6,7 @@ if [ ${DATASET} = "tinyimagenet" ];then
 fi 
 ALPHA=0.1
 NBITS=4
-DEVICE=2
+DEVICE=1
 
 python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQV9_"$ALPHA"_"B$NBITS" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
