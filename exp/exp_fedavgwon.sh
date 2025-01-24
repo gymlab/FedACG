df98a7315a05f0a87845ede4c7aaaa6dbe9f87a7
gymlab <swkim@pknu.ac.kr>
2025-01-24 15:03:43 +0900

    updates

--- exp/exp_fedavgwon.sh
+++ exp/exp_fedavgwon.sh
@@ -1,12 +1,13 @@
 CUDA_VISIBLE_DEVICES=0
-DATASET=cifar100
+DATASET=tinyimagenet
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-ALPHA=0.3
+ALPHA=0.6
+DEVICE=1
 
-python3 federated_train.py client=Won server=base exp_name=FedAvgWon_"$ALPHA" \
+python3 federated_train.py client=Won server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWon_iid \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
-batch_size=${BATCH_SIZE} wandb=False model=resnet18_Won project="dev_quant" \
-# split.mode=iid
+batch_size=${BATCH_SIZE} wandb=True model=resnet18_Won project="dev_quant" \
+split.mode=iid
