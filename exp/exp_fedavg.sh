8afce2e89f8ccdc775ee6242fa88fef7eb53a2f4
jbnu-vilab <seholee@jbnu.ac.kr>
2025-02-03 19:18:04 +0900

    updates

--- exp/exp_fedavg.sh
+++ exp/exp_fedavg.sh
@@ -1,12 +1,13 @@
-CUDA_VISIBLE_DEVICES=0
-DATASET=cifar100
+DATASET=cifar10
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-ALPHA=0.05
+ALPHA=0.3
+DEVICE=2
 
-python3 federated_train.py client=base server=base exp_name=FedAvg_"$ALPHA" \
+python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvg_"$ALPHA" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
-batch_size=${BATCH_SIZE} wandb=False model=resnet18_WS project="ablations_hyper" \
-split.mode=iid
+batch_size=${BATCH_SIZE} wandb=True project="FedWS_5_100" save_freq=500 \
+# split.mode=iid
+                               
