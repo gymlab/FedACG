e0f574f4448f0e978f741190e50478eb31579437
gymlab <swkim@pknu.ac.kr>
2025-02-04 17:38:32 +0900

    updates

---
+++ exp/exp_fedavgwsqg.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=0
+DATASET=tinyimagenet
+BATCH_SIZE=50
+if [ ${DATASET} = "tinyimagenet" ];then
+    BATCH_SIZE=100
+fi 
+ALPHA=0.1
+NBITS=4
+DEVICE=2
+
+python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQG_"$ALPHA"_"B$NBITS" \
+dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
+quantizer=WSQG quantizer.wt_bit=${NBITS} \
+batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant3" \
+# split.mode=iid
+# quantizer=WSQ quantizer.wt_bit=${NBITS} \
+# python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQ_"$ALPHA"_"B$NBITS" \
--- utils/qunat_function.py
+++ utils/qunat_function.py
@@ -165,7 +165,7 @@ class WSQConv2d(nn.Module):
         self.edges = 0.5 * (self.q_values[1:] + self.q_values[:-1])
         self.clip_prob = clip_prob
     
-    def forward(self, x, global_x):
+    def forward(self, x, global_x, std):
         with torch.no_grad():
             x = x - global_x    # residual
             
@@ -202,7 +202,74 @@ def WSQ_update(model, global_model, args):
                 param.data.copy_(layer_quant_conv(param.data, g_params[name].data)) 
             elif "downsample.0.weight" in name:
                 quant_conv1x1 = WSQConv2d(n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
-                param.data.copy_(quant_conv1x1(param.data, g_params[name].data)) 
+                param.data.copy_(quant_conv1x1(param.data, g_params[name].data))
+                
+
+class WSQGConv2d(nn.Module):
+    bit4 = [-2.6536, -1.9735, -1.508, -1.149, -0.8337, -0.5439, -0.2686, 0.,
+            0.2303, 0.4648, 0.7081, 0.9663, 1.2481, 1.5676, 1.9679, 2.6488]
+
+    def __init__(self, n_bits=1, clip_prob=0.001):
+        super(WSQGConv2d, self).__init__()
+        
+        q_values = torch.tensor(getattr(self, f'bit{n_bits}'), dtype=torch.float32)
+        self.q_values = torch.sort(q_values).values
+        self.edges = 0.5 * (self.q_values[1:] + self.q_values[:-1])
+        self.clip_prob = clip_prob
+    
+    def forward(self, x, global_x, std):
+        with torch.no_grad():
+            x = x - global_x    # residual
+            local_std = x.std()
+            x = x / std
+            # x = x_clipped / clip_threshold
+
+            indices = torch.bucketize(x, self.edges, right=False)
+            quantized_x = self.q_values[indices]
+            dequantized_x = std * quantized_x
+            
+            updated_global_x = global_x + dequantized_x
+            
+        return updated_global_x, local_std
+
+
+def WSQG_update(model, global_model, args):
+    
+    g_params = dict(global_model.named_parameters())
+    
+    global_std_values = {}
+    for name, param in global_model.named_parameters():
+        if "global_std" in name:
+            # 앞부분이 같은 "weight" 키를 찾아서 std를 가져옴
+            weight_name = name.replace("global_std", "weight")
+            global_std_values[weight_name] = param.data    # weight에서 저장한 std 값 가져오기
+
+    local_std_values = {}
+    for name, param in model.named_parameters():
+        if hasattr(args.quantizer, 'keyword'):
+            if 'first-last' in args.quantizer.keyword and name == 'conv1.weight':
+                first_quant_conv = WSQGConv2d(n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
+                updated_param, local_std = first_quant_conv(param.data, g_params[name].data, global_std_values[name])
+                param.data.copy_(updated_param)
+                local_std_values[name] = local_std
+            elif name != "conv1.weight" and ("conv1.weight" in name or "conv2.weight" in name):
+                layer_quant_conv = WSQGConv2d(n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
+                updated_param, local_std = layer_quant_conv(param.data, g_params[name].data, global_std_values[name])
+                param.data.copy_(updated_param)
+                local_std_values[name] = local_std
+            elif "downsample.0.weight" in name:
+                quant_conv1x1 = WSQGConv2d(n_bits=args.quantizer.wt_bit, clip_prob=args.quantizer.wt_clip_prob)
+                updated_param, local_std = quant_conv1x1(param.data, g_params[name].data, global_std_values[name])
+                param.data.copy_(updated_param)
+                local_std_values[name] = local_std
+
+    for name, param in model.named_parameters():
+        if "local_std" in name:
+            # 앞부분이 같은 "weight" 키를 찾아서 std를 가져옴
+            weight_name = name.replace("local_std", "weight")
+            if weight_name in local_std_values:
+                std = local_std_values[weight_name]  # weight에서 저장한 std 값 가져오기
+                param.data.copy_(std)
 
                
 def quantize_and_dequantize(tensor, global_tensor, bit_width, lr=1.0, flag = False, quantization_weight=None):
