eb4a48d54c984deedbdf2ba79c392133cd09412b
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-02-07 21:26:31 +0900

    updates

--- exp/exp_fedavgwsqg.sh
+++ exp/exp_fedavgwsqg.sh
@@ -1,17 +1,16 @@
-CUDA_VISIBLE_DEVICES=0
 DATASET=cifar10
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-ALPHA=0.1
+ALPHA=0.3
 NBITS=4
-DEVICE=3
+DEVICE=1
 
-python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGV3_0.2_"$ALPHA"_"B$NBITS" \
+python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGV3_0.1_iid_"B$NBITS" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
-quantizer=WSQG quantizer.wt_bit=${NBITS} quantizer.momentum=0.2 quantizer.wt_clip_prob=-1 \
+quantizer=WSQG quantizer.wt_bit=${NBITS} quantizer.momentum=0.1 quantizer.wt_clip_prob=0.001 \
 batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant3" \
-# split.mode=iid
+split.mode=iid
 # quantizer=WSQ quantizer.wt_bit=${NBITS} \d
 # python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQ_"$ALPHA"_"B$NBITS" \
