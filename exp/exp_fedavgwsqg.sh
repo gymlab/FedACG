434c31227485fbfec9969f9097ee2ae002ec6e59
jbnu-vilab <seholee@jbnu.ac.kr>
2025-02-07 21:06:07 +0900

    updates

--- exp/exp_fedavgwsqg.sh
+++ exp/exp_fedavgwsqg.sh
@@ -1,21 +1,15 @@
-DATASET=cifar10
+DATASET=cifar100
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-ALPHA=0.3
-NBITS=4
+ALPHA=0.1
+NBITS=2
 DEVICE=3
 
-<<<<<<< HEAD
-python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGV1_0.5_"$ALPHA"_"B$NBITS" \
+python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGV1_"$ALPHA"_"B$NBITS" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
-quantizer=WSQG quantizer.wt_bit=${NBITS} quantizer.momentum=0.5 quantizer.wt_clip_prob=-1 \
-=======
-python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGV3_0.2_"$ALPHA"_"B$NBITS" \
-dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
-quantizer=WSQG quantizer.wt_bit=${NBITS} quantizer.momentum=0.2 quantizer.wt_clip_prob=-1 \
->>>>>>> 51bea890ae78495d38b0ff2c1ea355e8c9f12572
+quantizer=WSQG quantizer.wt_bit=${NBITS} quantizer.momentum=0.1 quantizer.wt_clip_prob=-1 \
 batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant3" \
 # split.mode=iid
 # quantizer=WSQ quantizer.wt_bit=${NBITS} \d
