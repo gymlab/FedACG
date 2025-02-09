09d17928d5f711b1ccb5e5b54c9d9d108277de58
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-02-09 13:06:29 +0900

    updates

--- exp/exp_fedavgwsqg.sh
+++ exp/exp_fedavgwsqg.sh
@@ -1,11 +1,11 @@
-DATASET=cifar10
+DATASET=cifar100
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-ALPHA=0.1
-NBITS=2
-DEVICE=3
+ALPHA=0.6
+NBITS=1
+DEVICE=1
 
 python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGV1_"$ALPHA"_"B$NBITS" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
