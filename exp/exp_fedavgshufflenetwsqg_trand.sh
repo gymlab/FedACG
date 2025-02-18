2dadf0fa24b152974ec59c53aafaf4ea9633a273
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-02-18 15:25:32 +0900

    updates

--- exp/exp_fedavgshufflenetwsqg_trand.sh
+++ exp/exp_fedavgshufflenetwsqg_trand.sh
@@ -1,15 +1,15 @@
-DATASET=cifar10
+DATASET=cifar100
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
 ALPHA=0.6
-DEVICE=1
+DEVICE=0
 
-python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGTRand_"$ALPHA" \
+python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgViTWSQGTRand_"$ALPHA" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
 quantizer=WSQG quantizer.random_bit=rand_alloc quantizer.momentum=0.1 quantizer.wt_clip_prob=-1 \
-batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant3" \
+batch_size=${BATCH_SIZE} wandb=True model=MobileViTWS project="dev_quant3" \
 # split.mode=iid
 # quantizer=WSQ quantizer.wt_bit=${NBITS}
 # python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQ_"$ALPHA"_"B$NBITS" \
