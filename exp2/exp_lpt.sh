450cf6090507cf17411d4d70601409ca36db71e7
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-04-09 18:06:56 +0900

    updates

--- exp2/exp_lpt.sh
+++ exp2/exp_lpt.sh
@@ -1,9 +1,8 @@
-CUDA_VISIBLE_DEVICES=0
-DATASET=cifar10
-MODEL=resnet18_WS_LPT
-ALPHA=0.1
+DATASET=cifar100
+MODEL=resnet18_LPT
+ALPHA=0.3
 
-for NBITS in 1 2 4 6 8
+for NBITS in 4 6 8 2
 do
     if [ "${DATASET}" = "tinyimagenet" ]; then
         BATCH_SIZE=100
@@ -11,9 +10,9 @@ do
         BATCH_SIZE=50
     fi
 
-    python3 federated_train.py client=base server=base exp_name="${MODEL}_${ALPHA}_B${NBITS}" \
+    python3 federated_train.py visible_devices="1" client=base server=base exp_name="${MODEL}_IID_B${NBITS}" \
     dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 model.moving_average=False \
     batch_size=${BATCH_SIZE} quantizer=LPT quantizer.quantization_bits=${NBITS} \
     wandb=True model=${MODEL} project="BMVC_2025"
-    # split.mode=iid
+    split.mode=iid
 done
