d89e0930c94c05f854bc1322c23e9971883dc87a
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-04-09 16:23:11 +0900

    updates

--- exp2/exp_lpt.sh
+++ exp2/exp_lpt.sh
@@ -14,6 +14,6 @@ do
     python3 federated_train.py client=base server=base exp_name="${MODEL}_${ALPHA}_B${NBITS}" \
     dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 model.moving_average=False \
     batch_size=${BATCH_SIZE} quantizer=LPT quantizer.quantization_bits=${NBITS} \
-    wandb=True model=${MODEL} project="LPT_WS_MERGE"
+    wandb=True model=${MODEL} project="BMVC_2025"
     # split.mode=iid
 done
