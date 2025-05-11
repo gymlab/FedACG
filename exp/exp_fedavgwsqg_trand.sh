24d06a697a044dc414cad3b36e7a184ae63312ae
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-05-11 09:59:36 +0900

    updates

--- exp/exp_fedavgwsqg_trand.sh
+++ exp/exp_fedavgwsqg_trand.sh
@@ -1,15 +1,15 @@
-DATASET=cifar10
+DATASET=tinyimagenet
 BATCH_SIZE=50
 if [ ${DATASET} = "tinyimagenet" ];then
     BATCH_SIZE=100
 fi 
-ALPHA=0.3
-DEVICE=1
+ALPHA=0.03
+DEVICE=0
 
-python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGTRand_iid \
+python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQGTRand_"$ALPHA" \
 dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
 quantizer=WSQG quantizer.random_bit=rand_alloc quantizer.momentum=0.1 quantizer.wt_clip_prob=-1 \
-batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="dev_quant3" \
-split.mode=iid
+batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="ICCV_REBUTTAL" \
+# split.mode=iid
 # quantizer=WSQ quantizer.wt_bit=${NBITS}
 # python3 federated_train.py client=base server=base visible_devices=\'$DEVICE\' exp_name=FedAvgWSQ_"$ALPHA"_"B$NBITS" \
