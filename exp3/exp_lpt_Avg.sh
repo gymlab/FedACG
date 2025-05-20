286115faff42c6eea63aa4fb0c271f05d693a7a9
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-05-20 12:53:20 +0900

    updates

--- exp3/exp_lpt_Avg.sh
+++ exp3/exp_lpt_Avg.sh
@@ -1,12 +1,20 @@
 DATASET=cifar10
 ALPHA=0.6
 
-cd /home/gymlab/projects/FedACG-LPT
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate fl
+
+cd ~/projects/LPT_FL/FedACG
 
 for MODEL in resnet18_WS_LPT resnet18_LPT
 do
     for NBITS in 6
     do
+        
         if [ "${DATASET}" = "tinyimagenet" ]; then
             BATCH_SIZE=100
 
@@ -15,10 +23,10 @@ do
 
         fi
 
-        EXP_NAME="${MODEL}_${ALPHA}_B${NBITS}_RCL"
-        
-        CUDA_VISIBLE_DEVICES=3 python3 federated_train.py \
-        client=fedrcl server=base \
+        EXP_NAME="${MODEL}_${ALPHA}_B${NBITS}_Avg"
+
+        CUDA_VISIBLE_DEVICES=0 python3 federated_train.py \
+        client=base server=base \
         exp_name="${EXP_NAME}" \
         dataset=${DATASET} \
         trainer.num_clients=100 \
