7740cc22781320df06a50ada38a85e0faed767a2
gymlab <swkim@pknu.ac.kr>
2025-05-28 18:09:55 +0900

    fw_bw_updates

--- exp3/exp_lpt.sh
+++ exp3/exp_lpt.sh
@@ -1,5 +1,5 @@
 DATASET=cifar10
-MODEL=resnet18_WS_LPT
+MODEL=resnet18_WS_LPT2
 ALPHA=0.1
 cd ..
 
@@ -11,9 +11,9 @@ do
         BATCH_SIZE=50
     fi
 
-    python3 federated_train.py visible_devices="0" client=base server=base exp_name="${MODEL}_iid_B${NBITS}_RAU_B" \
+    python3 federated_train.py visible_devices="1" client=base server=base exp_name="${MODEL}_IID_B${NBITS}_uni_WAG_fw_bw_uni" \
     dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 model.moving_average=False \
-    batch_size=${BATCH_SIZE} quantizer=LPT quantizer.quantization_bits=${NBITS} quantizer.block_dim="B"  \
+    batch_size=${BATCH_SIZE} quantizer=LPT quantizer.quantization_bits=${NBITS} quantizer.small_block="None"  \
     wandb=True model=${MODEL} project="BMVC_2025_3" model.init_mode='kaiming_uniform' quantizer.uniform_mode='BFP' \
     split.mode=iid
 done
