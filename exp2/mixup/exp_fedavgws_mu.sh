307c1d1be5caddbca9a207fb74cf0b187fd90d80
Kim-Jiah <whatime08@pukyong.ac.kr>
2025-07-21 22:34:49 +0900

    updates

--- exp2/mixup/exp_fedavgws_mu.sh
+++ exp2/mixup/exp_fedavgws_mu.sh
@@ -9,6 +9,6 @@ MU_PROB=0.2
 
 python3 federated_train.py visible_devices=\'$DEVICE\' client=base server=base exp_name=FedAvgWS_mu"$MU_PROB"_"$ALPHA" \
  dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
- dataset.cutmix.use=true dataset.cutmix.cutmix_reg=true dataset.cutmix.prob=${MU_PROB} \
+ dataset.mixup.use=true dataset.mixup.mixup_reg=true dataset.mixup.prob=${MU_PROB} \
  batch_size=${BATCH_SIZE} wandb=True model=resnet18_WS project="ICLR" \
 # split.mode=iid
