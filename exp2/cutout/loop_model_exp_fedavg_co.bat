fd0c56ed1ce85e89efdad1fb38d66a473f5af03b
Kim-Jiah <whatime08@pukyong.ac.kr>
2025-08-12 16:54:50 +0900

    model_exp

---
+++ exp2/cutout/loop_model_exp_fedavg_co.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set BATCH_SIZE=50
+set CUDA_VISIBLE_DEVICES=0
+set DATASET=cifar10
+set ALPHA=0.3
+set CO_PROB=0.2
+cd ../..
+
+for %%M in (MobileViT VGG9_base ShuffleNet_base SqueezeNet_base) do (
+    set EXP_NAME=FedAvg_%%M_co%CO_PROB%_%ALPHA%_num1
+
+    if "%DATASET%"=="tinyimagenet" (
+        set BATCH_SIZE=100
+    ) else (
+        set BATCH_SIZE=50
+    )
+    echo Running experiment for %DATASET%
+
+    python federated_train.py client=base server=base ^
+        visible_devices='%CUDA_VISIBLE_DEVICES%' ^
+        exp_name=!EXP_NAME! ^
+        dataset=%DATASET% ^
+        trainer.num_clients=100 ^
+        split.alpha=%ALPHA% ^
+        trainer.participation_rate=0.05 ^
+        batch_size=!BATCH_SIZE! ^
+        wandb=True ^
+        model=%%M ^
+        project="ICLR" ^
+        dataset.cutout.use=True ^
+        dataset.cutout.use_reg=True ^
+        dataset.cutout.prob=%CO_PROB%
+)
+endlocal
---
+++ exp2/new_cutmixup/loop_model_exp_fedprox_ncmu.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set CM_PROB=0.1
+set MU_PROB=0.1
+set BATCH_SIZE=50
+set CUDA_VISIBLE_DEVICES=0
+set DATASET=cifar10
+set ALPHA=0.3
+cd ../..
+
+for %%M in (MobileViT VGG9_base ShuffleNet_base SqueezeNet_base) do (
+    set EXP_NAME=FedProx_%%M_ncmu%CM_PROB%_%MU_PROB%_%ALPHA%_num1
+
+    if "%DATASET%"=="tinyimagenet" (
+        set BATCH_SIZE=100
+    ) else (
+        set BATCH_SIZE=50
+    )
+    echo Running experiment for %DATASET%
+
+    python federated_train.py client=Prox server=base ^
+        visible_devices='%CUDA_VISIBLE_DEVICES%' ^
+        exp_name=!EXP_NAME! ^
+        dataset=%DATASET% ^
+        trainer.num_clients=100 ^
+        split.alpha=%ALPHA% ^
+        trainer.participation_rate=0.05 ^
+        batch_size=!BATCH_SIZE! ^
+        wandb=True ^
+        model=%%M ^
+        project="ICLR" ^
+        dataset.new_cutmixup.use=True ^
+        dataset.new_cutmixup.use_reg=True ^
+        dataset.new_cutmixup.cutmix_prob=%CM_PROB% ^
+        dataset.new_cutmixup.mixup_prob=%MU_PROB%
+)
+
+endlocal
