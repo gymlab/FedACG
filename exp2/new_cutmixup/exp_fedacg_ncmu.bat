37733a507641885967b28692f721ab93762e0714
Seung-Wook Kim <swkim@pknu.ac.kr>
2025-11-10 15:41:14 +0900

    rcl&acg exp

---
+++ exp2/new_cutmixup/exp_fedacg_ncmu.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set CUDA_VISIBLE_DEVICES=0
+set DATASET=cifar10
+set MODEL=resnet18
+set ALPHA=0.1
+set CM_PROB=0.15
+set MU_PROB=0.15
+cd ../..
+
+
+if "%DATASET%"=="tinyimagenet" (
+    set BATCH_SIZE=100
+) else (
+    set BATCH_SIZE=50
+)
+
+python federated_train.py client=ACG server=FedACG ^
+    visible_devices='%CUDA_VISIBLE_DEVICES%' ^
+    exp_name="FedACG_ncmu%CM_PROB%_%MU_PROB%_%ALPHA%_num1" ^
+    dataset=%DATASET% ^
+    trainer.num_clients=100 ^
+    split.alpha=%ALPHA% ^
+    split.mode=iid ^
+    seed=3 ^
+    trainer.participation_rate=0.05 ^
+    batch_size=!BATCH_SIZE! ^
+    wandb=True ^
+    model=%MODEL% ^
+    project="ICLR" ^
+    dataset.new_cutmixup.use=True ^
+    dataset.new_cutmixup.use_reg=True ^
+    dataset.new_cutmixup.cutmix_prob=%CM_PROB% ^
+    dataset.new_cutmixup.mixup_prob=%MU_PROB% 
+pause
+
---
+++ exp2/new_cutmixup/exp_fedacg_ncmu.sh
@@ -0,0 +1,16 @@
+DEVICE=0
+DATASET=tinyimagenet
+BATCH_SIZE=50
+if [ ${DATASET} = "tinyimagenet" ];then
+    BATCH_SIZE=100
+fi 
+ALPHA=0.3
+CM_PROB=0.15
+MU_PROB=0.15
+
+python3 federated_train.py visible_devices=\'$DEVICE\' client=ACG server=FedACG exp_name=FedACG_ncmu"$CM_PROB"_"$MU_PROB"_"$ALPHA"_num1 \
+ dataset=${DATASET} trainer.num_clients=100 split.alpha=${ALPHA} trainer.participation_rate=0.05 \
+ dataset.new_cutmixup.use=true dataset.new_cutmixup.use_reg=true \
+ dataset.new_cutmixup.cutmix_prob=${CM_PROB} dataset.new_cutmixup.mixup_prob=${MU_PROB} \
+ batch_size=${BATCH_SIZE} wandb=True model=resnet18 project="ICLR" seed=3 \
+#  split.mode=iid
